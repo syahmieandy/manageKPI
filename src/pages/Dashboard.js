6b01b3e3c203d87df0e28fa8c3a6651640c937ad
minmin2104 <aminkafri0045@gmail.com>
2026-05-01 11:13:34 +0800

    modify .primary-btn: index.css & add placeholder button: Dashboard.js

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,3 +1,21 @@
+import { useNavigate } from "react-router-dom";
+
+// author: amin
+// Placeholder je ni
+function ManageKPIButton() {
+  const navigate = useNavigate();
+  return (
+    <button
+      onClick={() => navigate("/kpi")}
+      type="button"
+      className="btn-primary"
+    >
+      Manage KPI
+    </button>
+  );
+}
+
 export default function Dashboard() {
-    return <div>Dashboard Page</div>;
-  }
+  return <ManageKPIButton />;
+}
+
